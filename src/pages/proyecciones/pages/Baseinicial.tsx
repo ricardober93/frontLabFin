8e26f98f2ab4ca269605bdb43233f23901d4b7b1
ricardober93 <ricardobzorro@gmail.com>
2021-06-13 17:09:52 +0000

    Configuration de la tablas para las proyecciones

--- src/pages/proyecciones/pages/Baseinicial.tsx
+++ src/pages/proyecciones/pages/Baseinicial.tsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React from "react";
+import TablePagination from "../components/TablePagination";
 
 export default function Baseinicial() {
-    return (
-        <div>
-            Soy Base inicial
-        </div>
-    )
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Activos",
+        columns: [
+          {
+            Header: "Nombre",
+            accessor: "name",
+          },
+          {
+            Header: "Valor",
+            accessor: "value",
+          },
+          {
+            Header: "",
+            accessor: "Acciones",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(
+    () => [
+      {
+        name: "line",
+        value: "field",
+      },
+      {
+        name: "hola",
+        value: "mundo",
+      },
+      {
+        name: "sofia",
+        value: "Marcena",
+      },
+      {
+        name: "Doña",
+        value: "Lau",
+      },
+      {
+        name: "Romero",
+        value: "Homero",
+      },
+      {
+        name: "Solo",
+        value: "on the dark",
+      },
+      {
+        name: "line",
+        value: "field",
+      },
+      {
+        name: "hola",
+        value: "mundo",
+      },
+      {
+        name: "sofia",
+        value: "Marcena",
+      },
+      {
+        name: "Doña",
+        value: "Lau",
+      },
+      {
+        name: "Romero",
+        value: "Homero",
+      },
+      {
+        name: "Solo",
+        value: "on the dark",
+      },
+      {
+        name: "line",
+        value: "field",
+      },
+      {
+        name: "hola",
+        value: "mundo",
+      },
+      {
+        name: "sofia",
+        value: "Marcena",
+      },
+      {
+        name: "Doña",
+        value: "Lau",
+      },
+      {
+        name: "Romero",
+        value: "Homero",
+      },
+      {
+        name: "Solo",
+        value: "on the dark",
+      },
+    ],
+    []
+  );
+
+  return (
+    <div>
+      Soy Base inicial
+      <TablePagination columns={columns} data={data} />
+    </div>
+  );
 }
