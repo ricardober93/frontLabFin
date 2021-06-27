96a443b706671b31a69aeab549ac4d7d1f2498d3
ricardober93 <ricardobzorro@gmail.com>
2021-06-27 13:08:34 +0000

    feat (nabvar): se organiza el nabvar y enlazan al router

--- src/component/Nabvar/Nabvar.tsx
+++ src/component/Nabvar/Nabvar.tsx
@@ -1,20 +1,28 @@
 import { Box, Button, Flex, Heading, Spacer } from '@chakra-ui/react'
 import { ColorModeSwitcher } from 'ColorModeSwitcher'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export const Nabvar = () => {
+  const history = useHistory()
+  const GotoRegister = () => {
+    history.push('register')
+  }
+  const GotoLogin = () => {
+    history.push('login')
+  }
     return (
         <Flex>
         <Box p="2">
-          <Heading size="md">Chakra App</Heading>
+          <Heading size="md">LabFin</Heading>
         </Box>
         <Spacer />
         <Box>
-          <Button colorScheme="teal" mr="4">
-            Sign Up
+          <Button onClick={GotoRegister} mr="4">
+            Registrase
           </Button>
-          <Button colorScheme="teal">Log in</Button>
-        <ColorModeSwitcher h="10%" justifySelf="flex-end" />
+          <Button onClick={GotoLogin} colorScheme="teal">Log in</Button>
+        <ColorModeSwitcher justifySelf="flex-end" />
         </Box>
       </Flex>
     )
