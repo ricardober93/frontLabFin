fbf1679968e2bbf08c28733c77308506aa5adc43
ricardober93 <ricardobzorro@gmail.com>
2021-07-17 21:57:34 +0000

    feat: se implementa el user context en la aplicacion

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import * as React from "react";
 import { Box, ChakraProvider, theme } from "@chakra-ui/react";
 import RouteWithSubRoutes from "./component/RouterWithSubRoutes";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { UserContext } from './context/UserContext';
 import routes from "./router";
 
-export const App = () => (
-  <Router>
-    <ChakraProvider theme={theme}>
-      <Box as="main" minH="100vh" p={3}>
-        <Switch>
-          {routes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route} />
-          ))}
-        </Switch>
-      </Box>
-    </ChakraProvider>
-  </Router>
-);
+export const App = () => {
+
+  const [user, setUser] = React.useState({ id: 0, email: '' });
+
+  return (
+
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <ChakraProvider theme={theme}>
+          <Box as="main" minH="100vh" p={3}>
+            <Switch>
+              {routes.map((route, i) => (
+                <RouteWithSubRoutes key={i} {...route} />
+              ))}
+            </Switch>
+          </Box>
+        </ChakraProvider>
+      </Router>
+    </UserContext.Provider>
+});
