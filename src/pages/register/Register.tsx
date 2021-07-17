02177c56a8ee3c843b5371e5a75dc93f10240b0c
ricardober93 <ricardobzorro@gmail.com>
2021-07-17 21:55:55 +0000

    feat: se crea la pagina de registro y su formulario, se aplica el servicio del register.service

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react';
+import MyApp from "../../component/layout/MyApp";
+import FormRegister from 'component/FormRegister/FormRegister';
+
+export default function Register() {
+  return (
+    <MyApp>
+      <Box
+        as="section"
+        minH="90vh"
+        w="80%"
+        m="0 auto"
+        d="flex"
+        gridGap="5"
+        justifyContent={{ base: "center" }}
+        alignItems="center" >
+        <FormRegister />
+      </Box>
+    </MyApp>
+  )
+}
