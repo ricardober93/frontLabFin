889c4e9dbe3540b5418de8813ada5f40e092168c
ricardober93 <ricardobzorro@gmail.com>
2021-07-20 20:07:04 +0000

    feat: se crean los interfaces para los datos

---
+++ src/pages/proyecciones/types/type.d.ts
@@ -0,0 +1,16 @@
+
+
+export interface Iactivos {
+    name: string;
+    valor: number;
+  }
+  
+  export interface Ipasivos {
+    name: string;
+    valor: number;
+  }
+  export interface Ipatrimonio {
+    name: string;
+    valor: number;
+  }
+  
