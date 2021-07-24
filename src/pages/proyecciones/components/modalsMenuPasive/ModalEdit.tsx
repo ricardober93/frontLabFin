d94ebedf055d9af185c641026e3b1d0468c40e8f
ricardober93 <ricardobzorro@gmail.com>
2021-07-24 22:24:37 +0000

    feat: se editan los pasivos y se cappturan los errores

--- src/pages/proyecciones/components/modalsMenuPasive/ModalEdit.tsx
+++ src/pages/proyecciones/components/modalsMenuPasive/ModalEdit.tsx
@@ -1,11 +1,43 @@
 import { EditIcon } from '@chakra-ui/icons'
-import { AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, FormControl, FormLabel, Input, MenuItem, useDisclosure } from '@chakra-ui/react'
-import React from 'react'
+import { AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, FormControl, FormLabel, Input, MenuItem, useDisclosure, useToast } from '@chakra-ui/react'
+import AlertMessage from 'component/AlertMessage'
+import { updatePasivoService } from 'pages/proyecciones/servicios/updatePasivo.Service'
+import React, { useState } from 'react'
 
-export default function ModalEditPasive({data}) {
+export default function ModalEditPasive({data,getAllPasivo}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const cancelRef = React.useRef()
-    console.log(data)
+    const newNamePasivo = React.useRef();
+    const newValorPasivo = React.useRef();
+    const [error, setError] = useState(false);
+    const toast = useToast();
+
+
+    const updatePasivo= () => {
+      updatePasivoService("/proyeccion/pasivo", data.id, {
+        name: newNamePasivo.current.value,
+        valor: newValorPasivo.current.value,
+      }).then((res) => {
+        if (res.status === 200) {
+          setTimeout(() => {
+            toast({
+              title: "Activo actualizado",
+              description: "se actualizo e activo correctamente",
+              status: "success",
+              duration: 9000,
+              isClosable: true,
+            });
+          }, 100);
+  
+          getAllPasivo();
+          onClose();
+        }
+  
+        if (res.status === 400) {
+          setError(true);
+        }
+      });
+    };
     return (
         <>
          <MenuItem  onClick={onOpen} icon={<EditIcon color="yellow.400" />}>
@@ -30,6 +62,7 @@ export default function ModalEditPasive({data}) {
           <Input
             type="text"
             defaultValue={data.name}
+            ref={newNamePasivo}
           />
         </FormControl>
 
@@ -38,16 +71,23 @@ export default function ModalEditPasive({data}) {
           <Input
             type="text"
             defaultValue={data.valor}
+            ref={newValorPasivo}
           />
         </FormControl>
-
+        {error ? (
+              <AlertMessage
+                status="error"
+                tittle="Error"
+                message="no se pudo actualizar el pasivo"
+              />
+            ) : null}
             </AlertDialogBody>
             <AlertDialogFooter>
               <Button ref={cancelRef} onClick={onClose}>
                 No
               </Button>
-              <Button colorScheme="red" ml={3}>
-                Yes
+              <Button colorScheme="yellow" ml={3} onClick={updatePasivo}>
+                Editar
               </Button>
             </AlertDialogFooter>
           </AlertDialogContent>
