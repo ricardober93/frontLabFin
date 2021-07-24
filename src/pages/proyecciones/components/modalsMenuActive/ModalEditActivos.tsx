167e3c17da3881c9a96859f947465ad7fd806e28
ricardober93 <ricardobzorro@gmail.com>
2021-07-24 22:23:32 +0000

    feat: se conecta la base de datos para actualizar el activo y se capturan los errores

--- src/pages/proyecciones/components/modalsMenuActive/ModalEditActivos.tsx
+++ src/pages/proyecciones/components/modalsMenuActive/ModalEditActivos.tsx
@@ -1,57 +1,106 @@
-import { EditIcon } from '@chakra-ui/icons'
-import { AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, FormControl, FormLabel, Input, MenuItem, useDisclosure } from '@chakra-ui/react'
-import React from 'react'
+import { EditIcon } from "@chakra-ui/icons";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  MenuItem,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import AlertMessage from "component/AlertMessage";
+import { updateActivoService } from "pages/proyecciones/servicios/updateActivo.service";
+import React from "react";
+import { useState } from "react";
 
-export default function ModalEditActivos({data}) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const cancelRef = React.useRef()
-    console.log(data)
-    return (
-        <>
-         <MenuItem  onClick={onOpen} icon={<EditIcon color="yellow.400" />}>
+export default function ModalEditActivos({ data, getAllActivo }: any) {
+  const toast = useToast();
+  const [error, setError] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
+  const newName = React.useRef();
+  const newValor = React.useRef();
+
+  const updateActivo = () => {
+    updateActivoService("/proyeccion/activo", data.id, {
+      name: newName.current.value,
+      valor: newValor.current.value,
+    }).then((res) => {
+      if (res.status === 200) {
+        setTimeout(() => {
+          toast({
+            title: "Activo actualizado",
+            description: "se actualizo e activo correctamente",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
+        }, 100);
+
+        getAllActivo();
+        onClose();
+      }
+
+      if (res.status === 400) {
+        setError(true);
+      }
+    });
+  };
+
+  return (
+    <>
+      <MenuItem onClick={onOpen} icon={<EditIcon color="yellow.400" />}>
         Editar
-          </MenuItem>
-        <AlertDialog
-          motionPreset="slideInBottom"
-          leastDestructiveRef={cancelRef}
-          onClose={onClose}
-          isOpen={isOpen}
-          isCentered
-        >
-          <AlertDialogOverlay />
-  
-          <AlertDialogContent>  
-            <AlertDialogHeader>Quiere modificar el activo</AlertDialogHeader>
-            <AlertDialogCloseButton />
-            <AlertDialogBody>
-              
-            <FormControl >
-          <FormLabel>Nombre</FormLabel>
-          <Input
-            type="text"
-            defaultValue={data.name}
-          />
-        </FormControl>
+      </MenuItem>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
 
-        <FormControl >
-          <FormLabel>Valor</FormLabel>
-          <Input
-            type="text"
-            defaultValue={data.valor}
-          />
-        </FormControl>
+        <AlertDialogContent>
+          <AlertDialogHeader>Quiere modificar el activo</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            <FormControl>
+              <FormLabel>Nombre</FormLabel>
+              <Input type="text" defaultValue={data.name} ref={newName} />
+            </FormControl>
 
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                No
-              </Button>
-              <Button colorScheme="red" ml={3}>
-                Yes
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </>
-    )
+            <FormControl>
+              <FormLabel>Valor</FormLabel>
+              <Input type="text" defaultValue={data.valor} ref={newValor} />
+            </FormControl>
+
+            {error ? (
+              <AlertMessage
+                status="error"
+                tittle="Error"
+                message="no se pudo actualizar el activo"
+              />
+            ) : null}
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              No
+            </Button>
+            <Button colorScheme="yellow" ml={3} onClick={updateActivo}>
+              Editar
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
