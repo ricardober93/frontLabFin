58466fd5460a63ba17e1cf61523d10be2e6595f2
ricardober93 <ricardobzorro@gmail.com>
2021-07-24 22:25:24 +0000

    feat: se elimina los patrimonio

--- src/pages/proyecciones/components/modalsMenuPatrimonio/ModalDeletePatrimonio.tsx
+++ src/pages/proyecciones/components/modalsMenuPatrimonio/ModalDeletePatrimonio.tsx
@@ -1,38 +1,82 @@
-import { DeleteIcon } from '@chakra-ui/icons'
-import { AlertDialog, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, MenuItem, useDisclosure } from '@chakra-ui/react'
-import React from 'react'
+import { DeleteIcon } from "@chakra-ui/icons";
+import {
+  AlertDialog,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  MenuItem,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { deletePatrimonioService } from "pages/proyecciones/servicios/deletePatrimonio.service";
+import React from "react";
 
-export default function ModalDeletePatrimonio({data}) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const cancelRef = React.useRef()
-    return (
-        <>
-        <MenuItem  onClick={onOpen} icon={<DeleteIcon color="red.400" />}>
-       Eliminar
-         </MenuItem>
-       <AlertDialog
-         motionPreset="slideInBottom"
-         leastDestructiveRef={cancelRef}
-         onClose={onClose}
-         isOpen={isOpen}
-         isCentered
-       >
-         <AlertDialogOverlay />
- 
-         <AlertDialogContent>  
-           <AlertDialogHeader>¿Esta seguro que queire eliminar el Patrimonio ?</AlertDialogHeader>
-           <AlertDialogCloseButton />
+export default function ModalDeletePatrimonio({ data, getAllPatrimonio }: any) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+  const toast = useToast();
 
-           <AlertDialogFooter>
-             <Button ref={cancelRef} onClick={onClose}>
-               No
-             </Button>
-             <Button colorScheme="red" ml={3}>
-               Eliminar el activo {data.name}
-             </Button>
-           </AlertDialogFooter>
-         </AlertDialogContent>
-       </AlertDialog>
-     </>
-    )
+  const deletePatrimonio = () => {
+    deletePatrimonioService("/proyeccion/patrimonio", data.id).then((res) => {
+      if (res.status === 200) {
+        setTimeout(() => {
+          toast({
+            title: "Activo eliminado",
+            description: "se elimine e activo correctamente",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
+        }, 100);
+        getAllPatrimonio();
+        onClose();
+      }
+
+      if (res.status === 400) {
+        toast({
+          title: "error",
+          description: "No se pudo eliminar el activo",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    });
+  };
+
+  return (
+    <>
+      <MenuItem onClick={onOpen} icon={<DeleteIcon color="red.400" />}>
+        Eliminar
+      </MenuItem>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            ¿Esta seguro que queire eliminar el Patrimonio ?
+          </AlertDialogHeader>
+          <AlertDialogCloseButton />
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              No
+            </Button>
+            <Button colorScheme="red" ml={3} onClick={deletePatrimonio}>
+              Eliminar el activo {data.name}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
