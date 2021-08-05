490f9f48a053bb7bfb4c470660a278e8df858fe4
ricardober93 <ricardobzorro@gmail.com>
2021-08-05 11:46:50 +0000

    feat: se crea el servicio de actualizar el salario

---
+++ src/Features/proyecciones/servicios/Salario/update.service.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const updateSalario = async (url: string, id: number, data: any) => {
+  try {
+    return await axios.put(`${url}/${id}`, data);
+  } catch (error) {
+    return error;
+  }
+};
